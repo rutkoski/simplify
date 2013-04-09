83d7a72cb5c84f1fa6887e611c01375478928f92
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2013-04-09 11:08:34 -0700

    Fixed a bug with zoom crop.

--- lib/Simplify/Thumb/Plugin/ZoomCrop.php
+++ lib/Simplify/Thumb/Plugin/ZoomCrop.php
@@ -36,18 +36,18 @@ class Simplify_Thumb_Plugin_ZoomCrop extends Simplify_Thumb_Plugin
   protected function process(Simplify_Thumb_Processor $thumb, $width = null, $height = null, $gravity = Simplify_Thumb::CENTER)
   {
     $image = $thumb->image;
-    
-    $temp = Simplify_Thumb_Functions::resize($image, $width, $height, true, false);
-    
-    $w0 = imagesx($temp);
-    $h0 = imagesy($temp);
-    
+
+    $thumb->image = Simplify_Thumb_Functions::resize($image, $width, $height, true, false);
+
+    $w0 = imagesx($thumb->image);
+    $h0 = imagesy($thumb->image);
+
     $w1 = empty($width) ? $w0 : $width;
     $h1 = empty($height) ? $h0 : $height;
-    
+
     if ($w0 == $w1 && $h0 == $h1)
-      return $this;
-    
+      return;
+
     switch ($gravity) {
       case Simplify_Thumb::TOP_LEFT :
       case Simplify_Thumb::LEFT :
@@ -63,7 +63,7 @@ class Simplify_Thumb_Plugin_ZoomCrop extends Simplify_Thumb_Plugin
       default :
         $x = floor($w0 - $w1) / 2;
     }
-    
+
     switch ($gravity) {
       case Simplify_Thumb::TOP_LEFT :
       case Simplify_Thumb::TOP :
@@ -79,8 +79,8 @@ class Simplify_Thumb_Plugin_ZoomCrop extends Simplify_Thumb_Plugin
       default :
         $y = floor($h0 - $h1) / 2;
     }
-    
-    $thumb->image = Simplify_Thumb_Functions::crop($temp, $x, $y, $w1, $h1);
+
+    $thumb->image = Simplify_Thumb_Functions::crop($thumb->image, $x, $y, $w1, $h1);
   }
 
 }
