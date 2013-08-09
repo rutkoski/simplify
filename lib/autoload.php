16921c53578d9302cd62b52cac64da65f1d4a62d
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2013-08-09 14:00:09 -0300

    Better detection of already loaded files.

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,16 +1,20 @@
 <?php
 
-if (! defined('SY_DIR')) {
-  define('SY_DIR', dirname(__file__));
-
+if (! function_exists('sy_autoload_register')) {
   require_once('functions.php');
+}
 
-  require_once ('Simplify/Autoload.php');
+if (! class_exists('Simplify_Autoload')) {
+  require_once ('Simplify' . DIRECTORY_SEPARATOR . 'Autoload.php');
 
   sy_autoload_register(array('Simplify_Autoload', 'autoload'));
 }
 
-require_once ('Simplify/Simplify.php');
+if (! defined('SY_DIR')) {
+  require_once ('Simplify/Simplify.php');
 
-Simplify_Autoload::registerPath(SY_DIR);
-Simplify_Autoload::registerPath(SY_DIR . '/vendor');
+  define('SY_DIR', dirname(__file__));
+
+  Simplify_Autoload::registerPath(SY_DIR);
+  Simplify_Autoload::registerPath(SY_DIR . '/vendor');
+}
